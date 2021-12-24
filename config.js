7b383d0493dd42c5cba12ca9619993ebac2d47fe
R0N1n-dev <mdrewj@live.com>
2021-12-24 15:06:57 +0300

    Initial commit ronin

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
-    mongoUri: process.env.MONGO_URI,
-    PORT: process.env.PORT || 3000,
-}
+  mongoUri:
+    process.env.MONGO_URI || "mongodb://localhost:27017/bucketListItems", //process.env.MONGO_URI,
+  PORT: process.env.PORT || 3000,
+};
